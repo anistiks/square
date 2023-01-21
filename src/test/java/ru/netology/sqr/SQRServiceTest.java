d1d2ec22b75bca10592bb94f80e6f4b0b435f3e3
test <anistiks@gmail.com>
2023-01-21 14:58:36 +0300

    add parametrizedTest

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,78 +1,87 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SQRServiceTest {
-    @Test
-    public void calcTestOne() {
+    @ParameterizedTest
+    @CsvSource({
+            "200,400,6",
+            "0,100,1",
+            "1,9,0",
+            "9801,10000,1",
+            "10,121,2",
+            "9,9604,89"
+    })
+    public void calcTest(int a, int b, int expected) {
         SQRService service = new SQRService();
 
-        int actual = service.calcSqr(200, 400);
-        int expected = 6;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void calcTestTwo() {
-        SQRService service = new SQRService();
-
-        int actual = service.calcSqr(0, 100);
-        int expected = 1;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void calcTestThree() {
-        SQRService service = new SQRService();
-
-        int actual = service.calcSqr(1, 9);
-        int expected = 0;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void calcTestFour() {
-        SQRService service = new SQRService();
-
-        int actual = service.calcSqr(9801, 10_000);
-        int expected = 1;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void calcTestFive() {
-        SQRService service = new SQRService();
-
-        int actual = service.calcSqr(10, 121);
-        int expected = 2;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void calcTestSix() {
-        SQRService service = new SQRService();
-
-        int actual = service.calcSqr(9, 9604);
-        int expected = 89;
-
+        int actual = service.calcSqr(a, b);
+        //int expected = 6;
         Assertions.assertEquals(expected, actual);
 
 
     }
 }
+
+//@Test
+//public void calcTestTwo() {
+//  //  SQRService service = new SQRService();
+//
+//    int actual = service.calcSqr(0, 100);
+//    int expected = 1;
+//
+//    Assertions.assertEquals(expected, actual);
+//
+//
+//}
+//
+//@Test
+//public void calcTestThree() {
+//    SQRService service = new SQRService();
+//
+//    int actual = service.calcSqr(1, 9);
+//    int expected = 0;
+//
+//    Assertions.assertEquals(expected, actual);
+//
+//
+//}
+//
+//@Test
+//public void calcTestFour() {
+//    SQRService service = new SQRService();
+//
+//    int actual = service.calcSqr(9801, 10_000);
+//    int expected = 1;
+//
+//    Assertions.assertEquals(expected, actual);
+//
+//
+//}
+//
+//@Test
+//public void calcTestFive() {
+//    SQRService service = new SQRService();
+//
+//    int actual = service.calcSqr(10, 121);
+//    int expected = 2;
+//
+//    Assertions.assertEquals(expected, actual);
+//
+//
+//}
+//
+//@Test
+//public void calcTestSix() {
+//    SQRService service = new SQRService();
+//
+//    int actual = service.calcSqr(9, 9604);
+//    int expected = 89;
+//
+//    Assertions.assertEquals(expected, actual);
+//
+//
+//}
+//
